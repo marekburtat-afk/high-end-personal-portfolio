6c155cdd0e4371169e5bbdb84faaf0ff7f133326
Marek Vertat <marekburtat@gmail.com>
2026-01-12 03:07:21 +0100

    Pridano Sanity Studio

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { Work } from './pages/Work';
 import { Blog } from './pages/Blog';
 import { ProjectDetail } from './pages/ProjectDetail';
+import { StudioPage } from './pages/StudioPage'; // To je ta nová stránka
 
 // Scroll to top wrapper
 const ScrollToTop = () => {
@@ -20,24 +21,31 @@ const App: React.FC = () => {
   return (
     <Router>
       <ScrollToTop />
-      <div className="min-h-screen bg-background text-white selection:bg-white selection:text-black font-sans flex flex-col">
-        <Navigation />
-        <main className="flex-grow pt-24 px-6 md:px-12 lg:px-24 max-w-7xl mx-auto w-full z-10 relative">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Work />} />
-            <Route path="/project/:slug" element={<ProjectDetail />} />
-            <Route path="/blog" element={<Blog />} />
-          </Routes>
-        </main>
-        <Footer />
+      <Routes>
+        {/* CESTA PRO STUDIO - musí být mimo hlavní design, aby nemělo menu a footer */}
+        <Route path="/studio/*" element={<StudioPage />} />
         
-        {/* Abstract Background Elements */}
-        <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-0 overflow-hidden opacity-20">
-            <div className="absolute top-[-10%] left-[-10%] w-[40vw] h-[40vw] bg-neutral-800 rounded-full blur-[120px]" />
-            <div className="absolute bottom-[-10%] right-[-10%] w-[30vw] h-[30vw] bg-neutral-900 rounded-full blur-[100px]" />
-        </div>
-      </div>
+        {/* HLAVNÍ WEB S MENU A FOOTEREM */}
+        <Route path="*" element={
+          <div className="min-h-screen bg-background text-white selection:bg-white selection:text-black font-sans flex flex-col">
+            <Navigation />
+            <main className="flex-grow pt-24 px-6 md:px-12 lg:px-24 max-w-7xl mx-auto w-full z-10 relative">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Work />} />
+                <Route path="/project/:slug" element={<ProjectDetail />} />
+                <Route path="/blog" element={<Blog />} />
+              </Routes>
+            </main>
+            <Footer />
+            
+            <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-0 overflow-hidden opacity-20">
+                <div className="absolute top-[-10%] left-[-10%] w-[40vw] h-[40vw] bg-neutral-800 rounded-full blur-[120px]" />
+                <div className="absolute bottom-[-10%] right-[-10%] w-[30vw] h-[30vw] bg-neutral-900 rounded-full blur-[100px]" />
+            </div>
+          </div>
+        } />
+      </Routes>
     </Router>
   );
 };
